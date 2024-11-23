446ee1bb013aeacced243ff1eeb1d06e40e1d5f5
Morikawa Hiroto M.H. <morikawa@tarot.local>
2024-11-24 00:45:07 +0900

    戦績記録画面と戦績表示画面を追加して遷移できるように

--- app/src/main/java/com/grumpjaw/pokepockettracker/fragment/ScoreFragment.kt
+++ app/src/main/java/com/grumpjaw/pokepockettracker/fragment/ScoreFragment.kt
@@ -5,12 +5,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.grumpjaw.pokepockettracker.R
+import com.grumpjaw.pokepockettracker.adapter.MenuAdapter
+import com.grumpjaw.pokepockettracker.viewmodel.MainMenuViewModel
 
 class ScoreFragment : Fragment() {
+    private val viewModel: MainMenuViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? = inflater.inflate(R.layout.fragment_score, container, false)
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView: RecyclerView = view.findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        val adapter =
+            MenuAdapter { menuItem ->
+                when (menuItem) {
+                    "戦績記録" -> findNavController().navigate(R.id.action_scoreFragment_to_scoreRecordFragment)
+                    "戦績表示" -> findNavController().navigate(R.id.action_scoreFragment_to_scoreDisplayFragment)
+                }
+            }
+        recyclerView.adapter = adapter
+
+        val menuItems = resources.getStringArray(R.array.score_menu_items).toList()
+        adapter.submitList(menuItems)
+    }
 }
---
+++ app/src/main/java/com/grumpjaw/pokepockettracker/fragment/ScoreRecordFragment.kt
@@ -0,0 +1,16 @@
+package com.grumpjaw.pokepockettracker.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.grumpjaw.pokepockettracker.R
+
+class ScoreRecordFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? = inflater.inflate(R.layout.fragment_score_record, container, false)
+}
